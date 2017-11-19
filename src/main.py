b0d137f51e57aa31bb674c848ec0d940fe255563
veetaw <veetaw@users.noreply.github.com>
2017-11-19 11:43:21 +0100

    SRC/MAIN.PY: first bot skeleton

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
 import telebot
 
-bot = telebot.TeleBot('')
+import config
+
+bot = telebot.TeleBot(config.BOT_TOKEN, skip_pending=True, num_threads=config.THREADS)
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
